1e88b55e58478e3f3c81a277f85cd2ff69b03e17
Sven <sven92.h@web.de>
2021-04-26 20:26:33 +0200

    add english welcome text

--- EmployeeDB-WenApi/Controllers/EmployeeController.cs
+++ EmployeeDB-WenApi/Controllers/EmployeeController.cs
@@ -17,15 +17,28 @@ namespace EmployeeDB_WenApi.Controllers
             _logik = logik;
         }
 
+        // ?lang=en
         [HttpGet]
-        public string Hello()
+        public string Hello(string lang = "de")
         {
-            return "Willkommen zum ASP.NET CORE Webservice von Sven Herrmann." +Environment.NewLine + 
-                   "Er besteht aus zwei Controllern, EmployeeController und den TestController um den Webservice zu testen." +Environment.NewLine + 
+            if (lang == "en")
+            {
+                return "Welcome to the ASP.NET CORE Webservice of Sven Herrmann" + Environment.NewLine +
+                   "I did implement two controller. EmployeeController and the TestController to check some Webservice functions." + Environment.NewLine +
+                   "You could use the calls below:" + Environment.NewLine +
+                   "1: ...'test/PostAddEmployees' Here it will create an new dummy employee and store it to the DB " + Environment.NewLine +
+                   "2: ...'test/PostAddEmployee/WithParameter' Here you can create an employee and give the parameters (string firstName, string lastName, string birthDate, string isActive)" + Environment.NewLine +
+                   "3: ...'test/DeleteEmployee/WithParameter' Here you can delete an employee that matches the given parameter id (Id)";
+            }
+            else
+            {
+                return "Willkommen zum ASP.NET CORE Webservice von Sven Herrmann." + Environment.NewLine +
+                   "Er besteht aus zwei Controllern, EmployeeController und den TestController um den Webservice zu testen." + Environment.NewLine +
                    "Sie können folgende Aufrufe machen:" + Environment.NewLine +
                    "1: ...'test/PostAddEmployees' Hier wird ein Dummy Employee angelegt " + Environment.NewLine +
                    "2: ...'test/PostAddEmployee/WithParameter' Hier kann man einen Employee mit Parametern anlegen (string firstName, string lastName, string birthDate, string isActive)" + Environment.NewLine +
                    "3: ...'test/DeleteEmployee/WithParameter' Hier wird der Employee gelöscht auf den die übergebene Id passt";
+            }
         }
 
         [HttpGet("GetEmployees")]
